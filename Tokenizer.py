c23900e44ef22d1ca779652b251677bb9d98a536
Thibault <thibault.lecordier91@gmail.com>
2022-01-03 15:00:13 +0100

    Traitement plusieurs phrases

--- Tokenizer.py
+++ Tokenizer.py
@@ -5,36 +5,53 @@ import re
 
 class Tokenizer:
 
-    def __init__(self, phrase):
-        self.phrase = phrase
+    def __init__(self, text):
+        self.text = text
         self.divide = []
         self.matrix = []
+        self.phrase = []
+
+
+    def separate_sentence(self):
+        self.phrase = re.split("[.?!]", self.text.lower())
+        for sentence in self.phrase:
+            if sentence == "":
+                self.phrase.remove(sentence)
+        #print(self.phrase)
 
     def separate(self):
         with open("./stopword.json") as f:
             stopwords = json.load(f)
+        for sentence in self.phrase:
+            self.divide.append(re.split("[,:;'() ]", sentence))
 
-        self.divide = re.split("[.,?:;'() ]", self.phrase.lower())
-
-        for word in stopwords:
-            for word2 in self.divide:
-                if word == word2 or word2 == '':
-                    self.divide.remove(word2)
+        for i in range(len(self.divide)):
+            for words in stopwords:
+                for word in self.divide[i]:
+                    if word == words or word == "":
+                        self.divide[i].remove(word)
 
     def count_word(self):
         lil_matrix = []
-        occurences = Counter(self.divide)
-        for word in occurences.keys():
-            lil_matrix.append(occurences[word])
-        self.matrix.append(lil_matrix)
+        repeat_list = []
+        for i in range(len(self.divide)):
+            repeat = Counter(self.divide[i])
+            repeat_list.append(repeat)
+
+        for repeat_word in repeat_list:
+            for word in repeat_word:
+                lil_matrix.append(repeat_word[word])
+            self.matrix.append(lil_matrix)
+            lil_matrix = []
 
     def show(self):
         print(self.divide)
         print(self.matrix)
 
 
-ph = "Mais pourqoui voulez-vous faire cela ? Je n'est jamais voulu faire parti de cette experience ni de cette interaction (test parathese). Je veux manger un tacos, mais, je n'est pas d'argent. On verra bien si le père Noel exist. Je veux manger japonais."
+ph = "Test test test, inutile donc indispensable. Oui mais non (ou peut-être)? Ceci me surprend veuillez arrêtez cette supercherie, je vous en remercie."
 ph1 = Tokenizer(ph)
+ph1.separate_sentence()
 ph1.separate()
 ph1.count_word()
 ph1.show()
